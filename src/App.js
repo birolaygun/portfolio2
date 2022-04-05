8f6cec1065e59e185b523d8882c6af0b78274b05
birol  aygun <birolaygun.bilisim@gmail.com>
2022-04-05 09:52:46 +0300

    nav works

--- src/App.js
+++ src/App.js
@@ -1,35 +1,159 @@
 import { useEffect, useRef, useState } from "react";
 import Mains from "./Mains";
-import ReactPageScroller from 'react-page-scroller';
+import { peace } from "./icons.js";
+import { FullPage, Slide } from "react-full-page";
 
 function App() {
-  const [red, green, blue] = [69, 111, 225];
+  const [navVisible, setNavVisible] = useState(false);
+  const [rVisible, setRVisible] = useState(false);
+  const [bVisible, setBVisible] = useState(false);
+  const [lVisible, setLVisible] = useState(false);
+  const [githubVisible, setGithubVisible] = useState(false);
+  const [youtubeVisible, setYoutubeVisible] = useState(false);
+  const [instaVisible, setInstaVisible] = useState(false);
+  const [twitterVisible, setLTwitterisible] = useState(false);
+  const [inVisible, setInVisible] = useState(false);
+
+  setTimeout(() => {
+    setNavVisible(true);
+    setTimeout(() => {
+      setBVisible(true);
+      setTimeout(() => {
+        setRVisible(true);
+        setTimeout(() => {
+          setLVisible(true);
+          setTimeout(() => {
+            setInVisible(true);
+            setTimeout(() => {
+              setLTwitterisible(true);
+              setTimeout(() => {
+                setInstaVisible(true);
+                setTimeout(() => {
+                  setYoutubeVisible(true);
+                  setTimeout(() => {
+                    setGithubVisible(true);
+                  }, 100);
+                }, 100);
+              }, 100);
+            }, 100);
+          }, 300);
+        }, 300);
+      }, 300);
+    }, 1300);
+  }, 3050);
 
   //bg color
 
   window.addEventListener("scroll", () => {
-    let y = 1 + (window.scrollY || window.pageYOffset) / 150;
-    y = y < 1 ? 1 : y; // ensure y is always >= 1 (due to Safari's elastic scroll)
-    const [r, g, b] = [red / y, green / y, blue / y].map(Math.round);
-    document.getElementById(
-      "firsBlock"
-    ).style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    let opacity = window.window.scrollY / window.innerHeight;
+    if (opacity > 1) {
+      opacity = 1;
+    }
+    document.getElementById("firsBlock").style.opacity = Math.abs(opacity - 1);
+    document.getElementById("secondBlock").style.opacity = Math.abs(
+      opacity - 1
+    );
   });
 
   return (
-    <div className="App ">
-      <button className=" right-0 z-[51] fixed" onClick={() => {}}>
-        btn
-      </button>
-      <nav className={`fixed bg-white w-screen h-24 z-50`}>nav here</nav>{" "}
-
-          <div
-            id="firsBlock"
-            className={`w-screen h-screen bg-blue flex items-center justify-center text-2xl font-extrabold`}
+    <div className="App scroll-m-96">
+      <nav
+        className={`fixed top-0 w-screen h-20 bg-white shadow-md z-50 animate-fadeInShadow animation-delay-3000 flex items-center justify-between px-5 ${
+          !navVisible && "invisible"
+        } `}
+      >
+        <div className="font- text-3xl font-extrabold drop-shadow-3xl text-white relative mb-9 cursor-pointer">
+          <span
+            className={` ${
+              !bVisible && "invisible"
+            } animate-fadeInFromLeft absolute animation-delay-4300 z-[54]`}
           >
-            <Mains />
+            B
+          </span>
+          <span
+            className={` ${
+              !rVisible && "invisible"
+            } animate-fadeInFromLeft absolute left-5 animation-delay-4600 z-[53]`}
+          >
+            R
+          </span>
+          <span
+            className={` ${
+              !lVisible && "invisible"
+            } animate-fadeInFromLeft absolute left-10 animation-delay-4900 z-[52]`}
+          >
+            L
+          </span>
+        </div>
+        <div className="space-x-4 flex mb-7">
+          <div className="w-7">
+            <a
+              className={`animate-fadeInDown absolute animation-delay-5100  ${
+                !inVisible && "invisible"
+              }`}
+              href="https://www.linkedin.com/in/birol-ayg%C3%BCn-89a346222"
+            >
+              <i className="fab fa-2x fa-linkedin-in"></i>{" "}
+            </a>
           </div>
-       
+          <div className="w-7">
+            <a
+              className={`animate-fadeInDown absolute animation-delay-5200 ${
+                !twitterVisible && "invisible"
+              }`}
+              href="https://twitter.com/BirolAygun2?t=gTKDKjr6X27lBL-L9pWHKA&s=09"
+            >
+              <i className="fab fa-2x fa-twitter"></i>
+            </a>
+          </div>
+          <div className="w-7">
+            <a
+              className={`animate-fadeInDown absolute animation-delay-5300 ${
+                !instaVisible && "invisible"
+              }`}
+              href="https://instagram.com/birolaygun2?utm_medium=copy_link"
+            >
+              <i className="fab fa-2x fa-instagram"></i>
+            </a>
+          </div>
+          <div className="w-8">
+            <a
+              className={`animate-fadeInDown absolute animation-delay-5400 ${
+                !youtubeVisible && "invisible"
+              }`}
+              href="https://www.youtube.com/channel/UCRnkf1wfBNBPRm7NoSf4PoA"
+            >
+              <i className="fab fa-youtube fa-2x"></i>{" "}
+            </a>
+          </div>
+          <div className="w-7">
+            <a
+              className={`animate-fadeInDown absolute animation-delay-5500  ${
+                !githubVisible && "invisible"
+              }`}
+              href="https://github.com/birolaygun"
+            >
+              <i className="fab fa-2x fa-github"></i>{" "}
+            </a>
+          </div>
+        </div>
+      </nav>
+
+      <FullPage>
+        <Slide>
+          <div className="w-screen h-screen flex bg-orange-600 relative">
+            <div id="firsBlock" className=" bg-white w-1/2 h-screen "></div>
+            <div id="secondBlock" className=" bg-black w-1/2 h-screen "></div>
+
+            <div className="absolute  w-0 h-screen left-1/2 flex justify-center animate-fadeInDown ">
+              <p className="rounded-full absolute  p-1 bg-white">{peace}</p>
+            </div>
+          </div>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+      </FullPage>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  
-    colors: {
-      transparant: "rgba(69, 111, 225, 0)",
-      blue: "rgb(69, 111, 225)",
-      white: "#FBF8F1",
+    // colors: {
+    //   transparant: "rgba(69, 111, 225, 0)",
+    //   blue: "rgb(69, 111, 225)",
+    //   white: "#FBF8F1",
+    // },Lucida Console
+    fontFamily: {
+      sans: ["Hind Siliguri", "sans-serif"],
+      title: ["Verdana", "sans-serif"],
+      Lucida: ["Lucida Console", "sans-serif"],
     },
     animationDelay: {
       11: "4000ms",
@@ -13,8 +17,21 @@ module.exports = {
       13: "12000ms",
       14: "16000ms",
       15: "20000ms",
+      3000: "3000ms",
+      4300: "4300ms",
+      4600: "4600ms",
+      4900: "4900ms",
+
+      5100: "5100ms",
+      5200: "5200ms",
+      5300: "5300ms",
+      5400: "5400ms",
+      5500: "5500ms"
     },
     extend: {
+      dropShadow: {
+        "3xl": "0 0 3px rgb(0, 0, 0)",
+      },
       keyframes: {
         show: {
           "0%": {
@@ -56,25 +73,55 @@ module.exports = {
         },
         fadeInDown: {
           "0%": {
-            opacity: 0,
-            transform: "translateY(-10%)",
+            transform: "translateY(100%)",
+          },
+          "100%": {
+            transform: "translateY(0%)",
+          },
+        },
+        fadeInShadow: {
+          "0%": {
+            opacity: 1,
+            transform: "scaleX(0);",
           },
           "100%": {
             opacity: 1,
-            transform: "translateY(0%)",
+            transform: "scelaX(1)",
+          },
+        },
+        fadeInFromLeft: {
+          "0%": {
+            // opacity: 0,
+            transform: "translateX(-100%)",
+          },
+          "100%": {
+            // opacity: 1,
+            transform: "translateX(0%)",
+          },
+        },
+        fadeInBigSmall: {
+          "0%": {
+            transform: "scela(0%)",
+          },
+          "70%": {
+            transform: "scela(150%)",
+          },
+          "100%": {
+            transform: "scela(100%)",
           },
         },
       },
       animation: {
         slideInLeft: "slideInLeft 500ms",
         fadeIn: "fadeIn 500ms",
-        fadeInDown: "fadeInDown 500ms",
+        fadeInDown: "fadeInDown 3000ms",
         show: "show 25000ms linear infinite",
+        fadeInShadow: "fadeInShadow 1000ms ",
+        fadeInFromLeft: "fadeInFromLeft 300ms ",
+        fadeInBigSmall: "fadeInBigSmall 1000ms ",
       },
     },
   },
 
-  plugins: [
-    require("tailwindcss-animation-delay"),
-  ],
+  plugins: [require("tailwindcss-animation-delay")],
 };
